2ee6664921de3c0a51f237ea58ab2095903b0f9e
Manon <manon.knoertzer@gmail.com>
2016-07-26 11:46:09 +0200

    Implemented the Split and Merge functions.
    There is still an issue with import of MutliwordToken (temporary fix in place).

--- scripts/TokenAggregate.js
+++ scripts/TokenAggregate.js
@@ -1,3 +1,10 @@
+
+// if using Node.js export module
+if (typeof exports !== 'undefined' && this.exports !== exports) {
+    var Token = require("../scripts/Token.js").Token;
+    var MultiwordToken = require("../scripts/MultiwordToken.js").MultiwordToken;
+
+}
 /**
  * A TokenAggregate is responsible to managing a collection of Tokens.
  * Specifically, it provides utilities for splitting and merging Tokens in an ordered list.
@@ -17,7 +24,7 @@
  * @constructor
  */
 var TokenAggregate = function(token_array) {
-    // Note: access the token array using: this[token_array]
+    // Note: access the token array using: this[token_array] (instead of this.token_array)
 
     /**
      * split splits a token into two tokens.
@@ -25,20 +32,67 @@ var TokenAggregate = function(token_array) {
      * For example, if we have tokens [{id: 1, form: 'token1'}, {id: 2, form:'token2'}],
      * calling split(1,2) would result in [{id: 1, form: 'to'},{id: 2, form:'ken1'}, {id: 3, form:'token2'}]
      * @param token_id
-     * @param index
+     * @param string_index
      */
-    this.split = function(token_id, index) {
-        // throw new Error("Not Implemented");
-        // TODO: implement
+    this.split = function(token_id, string_index) {
+        var found = false;
+        for (var x=0; x< this[token_array].length; x++) {
+            if (found === true) {
+                var MultiwordToken = require("../scripts/MultiwordToken.js").MultiwordToken; //TODO: figure out how to move this
+                if (!(this[token_array][x] instanceof MultiwordToken)) {
+                    this[token_array][x].id++; //gives cheeky +1 to the id's coming after the split. Makes space for our split (at least in id domain)
+                }
+                else {
+                    this[token_array][x].tokens.forEach(function (child) {
+                        child.id++; //updates the id's of the children in every multi-word token.
+                        //the parent in a multi-word token updates automatically based on the children.
+                    });
+                }
+            }
+            else if (this[token_array][x].id === token_id) {
+                var splitter = this[token_array][x];//find the splitter who is at the given token_id & assign to variable
+                var word = new Token(); // makes the new word an instance of Token
+                word.id = token_id;
+                word.form = splitter.form.slice(string_index);
+                splitter.form = splitter.form.slice(0, string_index);
+                this[token_array].splice((Number(x) + 1), 0, word);// inserts new word at the correct index in the array
+                found = true;
+            }
+
+        }
     };
 
+
+
     /**
-     * merge undoes a split when called on the same token_id.
+     * merge removes the next word and links it to the current word.
+     * For example, if we have [{id: 1, form: 'to'},{id: 2, form:'ken1'}, {id: 3, form:'token2'}]
+     * calling merge(1) would result in [{id:1, form: 'token1'}, {id: 2, form:'token2'}]
      * @param token_id
      */
     this.merge = function(token_id) {
-        throw new Error("Not Implemented");
-        // TODO: implement
+        found = false;
+        for (var x in this[token_array]) {
+            if (found === true) {
+                var MultiwordToken = require("../scripts/MultiwordToken.js").MultiwordToken; //TODO: figure out how to move this
+                if (!(this[token_array][x] instanceof MultiwordToken)) {
+                    this[token_array][x].id = this[token_array][x].id - 1; //updates the id's of every token after the merge
+                }
+                else {
+                    this[token_array][x].tokens.forEach(function (child) {
+                        child.id = child.id - 1; //updates the id's of the children in every multi-word token.
+                        //the parent in a multi-word token updates automatically based on the children.
+                    });
+                }
+            }
+            else if (this[token_array][x].id === token_id) {
+                var merger = this[token_array][x];
+                var gone = this[token_array][Number(x) + 1];
+                merger.form = merger.form + gone.form;
+                this[token_array].splice((Number(x) + 1), 1);
+                found = true;
+            }
+        }
     };
 };
 
