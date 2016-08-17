d31c720c716d792328d4b69f22ea9a25149d5613
Magdalena <gwinnie32@gmail.com>
2016-08-17 12:04:43 +0200

    reorganized ractive modules. fixed mwt split and merge bug

--- app/index.js
+++ app/index.js
@@ -46,18 +46,91 @@ var conllu_object = createConlluFromText("Lorem ipsum dolor sit amet, consectetu
 
 var conllu_object = createConlluFromText("I haven't a clue. They buy and sell books.");
 
+var token = Ractive.extend({
+    template: '#token',
+    isolated: true,
+    oninit: function () {
+        this.on('split', function (event) {
+
+            // call split(token_id, index)
+            console.log('split('+this.get('object').id+','+event.caretPosition+')');
+            this.parent.get('object').split(this.get('object').id, event.caretPosition);
+
+            // refresh the model
+            this.parent.updateModel();
+
+            // place the caret
+            var nextForm = event.node.parentNode.nextSibling.childNodes[2];
+            nextForm.focus();
+            nextForm.setSelectionRange(0,0);
+        });
+
+        this.on('merge', function (event) {
+            if(event.caretPosition===event.textLength) {
+
+                // call merge(token_id)
+                console.log("merge("+this.get('object').id+")");
+                this.parent.get('object').merge(this.get('object').id);
+
+                // refresh the model
+                this.parent.updateModel();
+
+                //place the caret
+                event.node.setSelectionRange(event.caretPosition,event.caretPosition);
+            }
+        });
+
+        this.on('backmerge', function (event) {
+            if(event.caretPosition===0) {
+                // find the token before the current token
+                var previousToken = event.node.parentNode.previousSibling;
+                var previousForm = previousToken.childNodes[2].value;
+                var previousId = previousToken.childNodes[0].textContent;
+
+                // if the id is a number, convert it to a number
+                if( !isNaN(+previousId )) {
+                    previousId = +previousId;
+                }
+
+                // call merge(previous_token_id)
+                console.log("merge("+previousId+")");
+                this.parent.get('object').merge(previousId);
+
+                // refresh the model
+                this.parent.updateModel();
+
+                // place the caret
+                previousToken.childNodes[2].focus();
+                previousToken.childNodes[2].setSelectionRange(previousForm.length,previousForm.length);
+            }
+        });
+
+        this.on('expand', function (event) {
+            console.log('expand('+this.get('object').id+','+event.caretPosition+')');
+            this.parent.get('object').expand(this.get('object').id,event.caretPosition);
+            this.parent.updateModel();
+        });
+    },
+    data: function () {
+        return {
+            object: new Token()
+        }
+    }
+});
+Ractive.components.token = token;
+
 var multiwordtoken = Ractive.extend({
     template: '#multiwordtoken',
     isolated: true,
     oninit: function () {
-        this.on('split', function (event, token) {
+        this.on('split', function (event) {
 
             // call split(token_id, index)
-            console.log('split('+token+','+event.caretPosition+')');
-            this.get('object').split(token, event.caretPosition);
+            console.log('split('+this.get('object').id+','+event.caretPosition+')');
+            this.parent.get('object').split(this.get('object').id, event.caretPosition);
 
             // refresh the model
-            this.updateModel();
+            this.parent.updateModel();
 
             // place the caret
             var nextForm = event.node.parentNode.nextSibling.childNodes[2];
@@ -67,15 +140,15 @@ var multiwordtoken = Ractive.extend({
         });
 
 
-        this.on('merge', function (event, token) {
+        this.on('merge', function (event) {
             if(event.caretPosition===event.textLength) {
 
                 // call merge(token_id)
-                console.log("merge("+token+")");
-                this.get('object').merge(token);
+                console.log("merge("+this.get('object').id+")");
+                this.parent.get('object').merge(this.get('object').id);
 
                 // refresh the model
-                this.updateModel();
+                this.parent.updateModel();
 
                 //place the caret
                 event.node.setSelectionRange(event.caretPosition,event.caretPosition);
@@ -107,8 +180,9 @@ var multiwordtoken = Ractive.extend({
             }
         });
 
-        this.on('collapse', function (event) {
-            this.parent.get('object').collapse(this.get('object').id);
+        this.on('collapse', function (event, token) {
+            console.log('collapse('+token+')');
+            this.parent.get('object').collapse(token);
             this.parent.updateModel();
         });
     },
@@ -116,6 +190,9 @@ var multiwordtoken = Ractive.extend({
         return {
             object: new Sentence()
         }
+    },
+    components: {
+        token: token
     },
     // Ractive's updateModel appears to have a bug in it. Until they fix it, we override their function with this
     updateModel: function() {
@@ -133,74 +210,6 @@ var sentence = Ractive.extend({
     template:'#sentence',
     isolated: true,
     oninit: function () {
-        this.on('split', function (event, token) {
-
-            // call split(token_id, index)
-            console.log('split('+token+','+event.caretPosition+')');
-            this.get('object').split(token, event.caretPosition);
-
-            // refresh the model
-            this.updateModel();
-
-            // place the caret
-            var nextForm = event.node.parentNode.nextSibling.childNodes[2];
-            nextForm.focus();
-            nextForm.setSelectionRange(0,0);
-
-        });
-
-
-        this.on('merge', function (event, token) {
-            if(event.caretPosition===event.textLength) {
-
-                // call merge(token_id)
-                console.log("merge("+token+")");
-                this.get('object').merge(token);
-
-                // refresh the model
-                this.updateModel();
-
-                //place the caret
-                event.node.setSelectionRange(event.caretPosition,event.caretPosition);
-            }
-        });
-
-        this.on('backmerge', function (event, token) {
-            if(event.caretPosition===0) {
-                // find the token before the current token
-                var previousToken = event.node.parentNode.previousSibling;
-                var previousForm = previousToken.childNodes[2].value;
-                var previousId = previousToken.childNodes[0].textContent;
-
-                // if the id is a number, convert it to a number
-                if( !isNaN(+previousId )) {
-                    previousId = +previousId;
-                }
-
-                // call merge(previous_token_id)
-                console.log("merge("+previousId+")");
-                this.get('object').merge(previousId);
-
-                // refresh the model
-                this.updateModel();
-
-                // place the caret
-                previousToken.childNodes[2].focus();
-                previousToken.childNodes[2].setSelectionRange(previousForm.length,previousForm.length);
-            }
-        });
-
-        this.on('expand', function (event, token) {
-            console.log('expand('+token+','+event.caretPosition+')');
-            this.get('object').expand(token,event.caretPosition);
-            this.updateModel();
-        });
-
-        this.on('collapse', function (event, token) {
-            console.log('collapse('+token+')');
-            this.get('object').collapse(token);
-            this.updateModel();
-        });
 
         this.on('splitComment', function (event, index) {
 
@@ -255,7 +264,8 @@ var sentence = Ractive.extend({
         }
     },
     components: {
-        multiwordtoken: multiwordtoken
+        multiwordtoken: multiwordtoken,
+        token: token
     },
     // Ractive's updateModel appears to have a bug in it. Until they fix it, we override their function with this
     updateModel: function() {
